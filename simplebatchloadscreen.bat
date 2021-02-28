a739e663d41b0aa50277cc0205440d4edfa4ceef
2147483647ms <77409841+2147483647ms@users.noreply.github.com>
2021-02-27 22:12:45 -0600

    Loading screen

---
+++ simplebatchloadscreen.bat
@@ -0,0 +1,42 @@
+@echo off 
+title Loading animation
+
+color 0c
+
+ping localhost -n 2 >nul
+cls
+echo #
+echo #
+
+ping localhost -n 2 >nul
+cls
+echo ##
+echo ##
+
+ping localhost -n 2 >nul
+cls
+echo ###
+echo ###
+
+ping localhost -n 2 >nul
+cls 
+echo ####
+echo ####
+
+ping localhost -n 2 >nul
+cls
+echo #####
+echo #####
+
+ping localhost -n 2 >nul
+cls
+echo ######
+echo ######
+
+ping localhost -n 2 >nul
+cls
+echo #######
+echo #######
+
+color 0f
+echo Done!
